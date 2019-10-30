b5bb9e699c6cb57a4fa4ac392ae092b2deb83de5
AJ Larson <aj.larson@stgconsulting.com>
2019-10-29 22:47:40 -0600

    updated

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,23 +1,45 @@
-const chrome = require("chromedriver")
-
+const chromedriver = require("chromedriver")
+const geckodriver = require("geckodriver")
 module.exports = {
-    "src_folders" : ["tests"],
-    "webdriver": {
-        "start_process": true,
-        "server_path": chrome.path,
-        "cli_args": [
-            "--verbose"
-        ],
-        "port": 9515
-    },
+    "src_folders": ["tests"],
+    // "page_objects_path": "pageObjects",
 
     "test_settings": {
         "default": {
+            "webdriver": {
+                "start_process": true,
+                "server_path": chromedriver.path,
+                "port": 9515
+            },
             "desiredCapabilities": {
                 "browserName": "chrome",
                 "chromeOptions": {
                     "w3c": false
                 }
+            },
+            "screenshots": {
+                "enabled": true, // just straight up turn screenshots on or off
+                "on_failure": true, // takes a screenshot when an assertion fails
+                "on_error": false, // if true, every time you have a command error out (like the wrong argument in a setValue) it will screenshot
+                "path": "screenshots" // this is a relative path to this file
+            }
+        },
+
+        "firefox": {
+            "webdriver": {
+                "start_process": true,
+                "server_path": geckodriver.path,
+                "port": 4444 //will fail on any other port
+            },
+            "desiredCapabilities": {
+                "browserName": "firefox",
+                "acceptInsecureCerts": true
+            },
+            "screenshots": {
+                "enabled": true,
+                "on_failure": true,
+                "on_error": false,
+                "path": "screenshots"
             }
         }
     }
--- tests/test.js
+++ tests/test.js
@@ -8,5 +8,16 @@ module.exports = {
     "Check title": browser=>{
         browser.waitForElementVisible("#hplogo", 10000)
             .verify.title("Google")
+    },
+    "Take a random screenshot": browser=>{
+        var path = process.cwd()  // process.cwd() is a method that Node gives us to get the current directory (where this project is)
+        browser.saveScreenshot(path + "/screenshots/randoScreenshot.png")
+    },
+    "Search puppies and force fail": browser=>{
+        browser.setValue("[name='q']", "puppies")
+            .waitForElementVisible("[name='btnK']", 5000)
+            .click("[name='btnK']")
+            .waitForElementVisible("#rso", 5000)
+            .assert.containsText("#rso", "puppies suck", "obviously this will fail because it isn't true.") //will generate a screenshot with the test name
     }
 }
